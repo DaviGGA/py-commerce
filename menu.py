9c7c4db7e5235cb645a9ded24beacc3d038040fe
Davi Gregório <106992602+DaviGGA@users.noreply.github.com>
2023-06-02 10:07:15 -0300

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,8 +1,13 @@
 import json
+import os
 
 def adiciona_ao_carrinho(produto):
 
     quantidade = int(input(f"Quantos {produto['nome']} você deseja comprar? "))
+
+    if quantidade <= 0:
+        print("Quantidade inválida.")
+        adiciona_ao_carrinho(produto)
 
     with open('carrinho.json', 'r') as file:
         carrinho = json.load(file)
@@ -16,7 +21,7 @@ def adiciona_ao_carrinho(produto):
 
     print(f"O produto {produto['nome']} foi adicionado ao carrinho!")
 
-    menu()
+    lista_produtos()
 
 
 def find(list,id):
@@ -69,7 +74,7 @@ def lista_produtos():
             print(f"Preço: R$ {produto['preco']}")
             print(f"Categoria: {produto['categoria']}")
 
-        option = input('Escolha o produto (ou digite "-" para sair): ')
+        option = input('Escolha o produto pelo Id (ou digite "-" para sair): ')
 
         if option == '-':
             menu()
@@ -80,9 +85,8 @@ def lista_produtos():
 
             if option == '1':
                 adiciona_ao_carrinho(produto)
-            if option == '2':
-                
-                print("Retornando ao menu.")
+            if option == '2':             
+              lista_produtos()
 
 
 def menu():
@@ -102,30 +106,3 @@ def menu():
 
 menu()
 
-# # Módulo para mexer com JSON
-
-# import json
-
-# # Lê o arquivo, e guarda o conteúdo na variável data
-
-# with open('data.json', 'r') as file:
-#     data = json.load(file)
-
-
-# # Criação do dicionario pessoa
-
-# pessoa = {
-#     'nome': "Chico",
-#     'idade': 20 
-#     }
-
-# # Insere a pessoa no array
-
-# data.append(pessoa)
-
-# # Abre o arquivo e insere o conteúdo da variável data no arquivo
-
-# with open('data.json', 'w') as file:
-#     json.dump(data, file)
-
-# print(data)
