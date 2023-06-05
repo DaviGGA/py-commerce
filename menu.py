1d3ff4e82702651c619fd3556ebb4eaf2a2e396d
Davi Gregório <106992602+DaviGGA@users.noreply.github.com>
2023-06-05 11:12:35 -0300

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,5 +1,24 @@
 import json
 import os
+from time import sleep
+
+def finalizar_compra(valor):
+    option = input("Tem certeza que deseja finalizar a compra? 1 - Sim 2 - Não\n")
+
+    if option == '1':
+        loading()
+
+
+        with open('carrinho.json', 'w') as file:
+            json.dump([], file)
+
+        print(f"Compra no valor de R$ {valor} finalizada com sucesso! Retornando ao menu...")
+        sleep(1.5)
+        os.system('cls')
+        menu()
+    elif option == '2':
+        loading()
+        ver_carrinho()
 
 def adiciona_ao_carrinho(produto):
 
@@ -32,6 +51,20 @@ def find(list,id):
     
     return p
 
+def loading():
+    carregando = 'carregando'
+    os.system('cls')
+    print(carregando)
+    sleep(0.25)
+   
+    for i in range(3):
+        os.system('cls')
+        carregando += '.'
+        print(carregando)
+        sleep(0.25)
+
+    os.system('cls')
+
 
 def calcula_carrinho(carrinho):
     valor = 0
@@ -40,10 +73,39 @@ def calcula_carrinho(carrinho):
 
     return valor
 
+def deleta_item_carrinho(id,carrinho):
+    
+    encontrou = False 
+    print(carrinho)
+    for produto in carrinho:
+        if produto['id'] == int(id):
+            carrinho.remove(produto)
+            print(carrinho)
+            with open('carrinho.json', 'w') as file:
+                json.dump(carrinho, file)
+            
+            encontrou = True
+
+    if encontrou:
+        print("Produto excluído com sucesso!")
+        sleep(2)
+        loading()
+        ver_carrinho()
+
+    else:
+        print("Desculpe, não encontramos o produto.")
+        sleep(2)
+        loading()
+        ver_carrinho()
+
 def ver_carrinho():
+    
     with open('carrinho.json', 'r') as file:
         carrinho = json.load(file)
-    
+
+    print('=' * 30)
+    print("CARRINHO".center(30))
+
     for produto in carrinho:
             print("=" * 30)
             print(f"Id: {produto['id']}")
@@ -58,7 +120,21 @@ def ver_carrinho():
 
     print(f"Valor do carrinho: R$ {valor}")
 
+    print("=" * 30)
+    print("1 - Finalizar compra")
+    print("2 - Excluir produto do carrinho")
+    print("3 - Voltar ao menu\n")
 
+    option = input('')
+
+    if option == '1':
+        finalizar_compra(valor)
+    elif option == '2':
+        id = input("Digite o Id do produto: ")
+        deleta_item_carrinho(id,carrinho)
+    elif option == '3':
+        loading()
+        menu()
 
     
 
@@ -77,6 +153,7 @@ def lista_produtos():
         option = input('Escolha o produto pelo Id (ou digite "-" para sair): ')
 
         if option == '-':
+            loading()
             menu()
         else:
             produto = find(produtos,option)
@@ -84,8 +161,10 @@ def lista_produtos():
             option = input(f"Deseja adicionar o produto {produto['nome']} ao carrinho? \n 1 - Sim 2 - Não \n")
 
             if option == '1':
+                loading()
                 adiciona_ao_carrinho(produto)
-            if option == '2':             
+            elif option == '2':
+              loading()             
               lista_produtos()
 
 
@@ -97,12 +176,18 @@ def menu():
     print("1 - Lista de produtos")
     print("2 - Carrinho")
     print("\n")
-    option = input()
 
-    if option == '1':
-        lista_produtos()
-    if option == '2':
+    op = input()
+
+    if op == '2':
+        loading()
         ver_carrinho()
+
+    elif op == '1':
+        loading()
+        lista_produtos()
+    
+  
 
 menu()
 
